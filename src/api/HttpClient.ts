5d5d7be265e77e94036ceeb5d17e4232b70b9880
filipe-machado <ax-filipe@hotmail.com>
2021-02-23 20:45:43 -0300

    👽 RAWG
    adicionando classe para configurar acesso ao axios

---
+++ src/api/HttpClient.ts
@@ -0,0 +1,20 @@
+import axios, { AxiosInstance } from 'axios';
+
+abstract class HttpClient {
+  protected readonly instance: AxiosInstance;
+
+  public constructor(
+    baseURL: string = process.env.REACT_APP_URL_API as string,
+    headers: Record<string, unknown> = {
+      Accept: 'application/json',
+      ContentType: 'application/json',
+    },
+  ) {
+    this.instance = axios.create({
+      baseURL,
+      headers,
+    });
+  }
+}
+
+export default HttpClient;
