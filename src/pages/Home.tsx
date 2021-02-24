4379bbf31cede0425d2fcc57d0cb275c6d74bd6f
filipe-machado <ax-filipe@hotmail.com>
2021-02-24 17:08:29 -0300

    🚧 RAWG
    removendo logo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import Layout from '../components/templates/Layout';
 import Presentation from '../components/templates/Presentation';
 
 const Home = (): React.ReactElement => (
-  <Layout logo="https://duckduckgo.com/i/3585ebbe.png">
+  <Layout>
     <Presentation />
   </Layout>
 );
