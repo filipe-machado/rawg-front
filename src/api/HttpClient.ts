760e622bef303d92a01c334e048e76f6cf84ff81
filipe-machado <ax-filipe@hotmail.com>
2021-02-24 20:24:12 -0300

    👽 RAWG
    adicionando variáveis de ambiente .env

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -3,18 +3,15 @@ import axios, { AxiosInstance } from 'axios';
 abstract class HttpClient {
   protected readonly instance: AxiosInstance;
 
-  private url = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_URL_API_PROD : process.env.REACT_APP_URL_API_DEV;
-
   public constructor(
-    url: string,
+    baseURL: string = process.env.REACT_APP_URL_API as string,
     headers: Record<string, unknown> = {
       Accept: 'application/json',
       ContentType: 'application/json',
     },
   ) {
-    this.url = url;
     this.instance = axios.create({
-      baseURL: this.url,
+      baseURL,
       headers,
     });
   }
